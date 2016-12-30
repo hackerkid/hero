45a073678d3beb5e288a7d76037db6886533faf4
hackerkid <hackerkid@vishnuks.com>
2016-12-30 16:35:49 +0530

    Add time tracker.

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onActivated.addListener(function(x) {
-  chrome.tabs.getSelected(null, function(tab) {
-    var currentURL = tab.url;
-  });
-});
---
+++ js/background.js
@@ -0,0 +1,99 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'XXX']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function clearStats() {
+  if (config.clearStatsInterval < 3600) {
+    config.nextTimeToClear = 0;
+    return;
+  }
+
+  if (!config.nextTimeToClear) {
+    var d = new Date();
+    d.setTime(d.getTime() + config.clearStatsInterval * 1000);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    if (config.clearStatsInterval > 3600) {
+      d.setHours(0);
+    }
+    config.nextTimeToClear = d.getTime();
+  }
+  var now = new Date();
+  if (now.getTime() > config.nextTimeToClear) {
+    sites.clear();
+    var nextTimeToClear = new Date(nextTimeToClear + config.clearStatsInterval * 1000);
+    config.nextTimeToClear = nextTimeToClear.getTime();
+    return;
+  }
+}
+
+var config = new Config();
+var sites = new Sites(config);
+var tracker = new Tracker(config, sites);
+
+/* Listen for requests which come from the user through the popup. */
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action == "clearStats") {
+      sites.clear();
+      sendResponse({});
+    } else if (request.action == "addIgnoredSite") {
+      config.addIgnoredSite(request.site);
+      sendResponse({});
+    } else {
+      console.log("Invalid action given: " + request.action);
+    }
+  });
+
+chrome.alarms.create("clearStats", {periodInMinutes: 2});
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (alarm.name == "clearStats") {
+    clearStats(config);
+  }
+});
+
+var config = new Config();
+var gsites = new Sites(config);
+
+function secondsToString(seconds) {
+  var years = Math.floor(seconds / 31536000);
+  var days = Math.floor((seconds % 31536000) / 86400);
+  var hours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+  var mins = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+  var secs = (((seconds % 31536000) % 86400) % 3600) % 60;
+  var s = "";
+  if (years) {
+    s = s + " " + years + "y";
+  }
+  if (days) {
+    s = s + " " + days + "d";
+  }
+  if (hours) {
+    s = s + " " + hours + "h";
+  }
+  if (mins) {
+    s = s + " " + mins + "m";
+  }
+  if (secs) {
+    s = s + " " + secs.toFixed(0) + "s";
+  }
+  return s;
+}
+
+/*
+chrome.tabs.onActivated.addListener(function(x) {
+  chrome.tabs.getSelected(null, function(tab) {
+    var currentURL = tab.url;
+    alert(secondsToString(gsites.sites[gsites.getSiteFromUrl(currentURL)]));
+  });
+});
+*/
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   alert(secondsToString(gsites.sites[gsites.getSiteFromUrl(changeInfo.url)]));
+}); 
